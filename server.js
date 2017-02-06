e62ebef17d20e74a224d4ccdd9064afb071c1bb1
durgeshkumar96 <kumardurgesh96@gmail.com>
2017-02-06 22:25:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ var articleOne = {
 };
 
 function createTemplate(data){ 
-    var title= data.title;
-    var date= data.date;
-    var heading= data.heading;
-    var content= data.content;
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
         
     var htmlTemplate = `
     <html>
